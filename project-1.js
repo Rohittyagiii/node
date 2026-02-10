45a6fcd9da168ba30d82eb83e96e0e26a46d7c6d
Rohittyagiii <tyagirohit698@gmail.com>
2026-02-10 22:06:23 +0530

    commit

--- project-1.js
+++ project-1.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const fs = require('fs');
 const users = require('./MOCK_DATA.json');
 const app = express();
 
-//routes
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+//routes
 app.get('/users', (req, res) => {
     const html = `
         <ul>
@@ -13,33 +17,38 @@ app.get('/users', (req, res) => {
     res.send(html);
 });
 
-//REST Api 
-
-app.get('/api/users', (req,res) => {
+// REST API
+app.get('/api/users', (req, res) => {
     return res.json(users);
 });
 
- app.route("/api/users/:id")
-   .get((req,res) => {
-      const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-   })
-   .patch((req,res) => {
-    return res.json({ status : "pending"})
-   })
-   .delete((req,res) => {
-    return res.json({ status : "pending"})
-   })
+app.route("/api/users/:id")
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find(user => user.id === id);
+        return res.json(user);
+    })
+    .patch((req, res) => {
+        return res.json({ status: "pending" });
+    })
+    .delete((req, res) => {
+        return res.json({ status: "pending" });
+    });
 
+app.post('/api/users', (req, res) => {
+    const body = req.body;
 
-app.post('./api/users', (req,res) => {
-    return res.json({status : "pending"});
+    users.push({ ...body, id: users.length + 1 });
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+            return res.json({ status: "error" });
+        }
+        return res.json({ status: "success", id: users.length });
+    });
 });
 
-
-
-//Server
+// Server
 app.listen(4000, () => {
     console.log('Server Started');
-})
+});
