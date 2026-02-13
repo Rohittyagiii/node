1ef040263d218dfe3aacfd2c0505f10b8e488582
Rohittyagiii <tyagirohit698@gmail.com>
2026-02-13 21:34:07 +0530

    commit

--- project-1.js
+++ project-1.js
@@ -37,6 +37,7 @@ app.route("/api/users/:id")
     .get((req, res) => {
         const id = Number(req.params.id);
         const user = users.find(user => user.id === id);
+        if(!user) return res.status(404).json({err: "User not foud"})
         return res.json(user);
     })
     .patch((req, res) => {
@@ -48,14 +49,16 @@ app.route("/api/users/:id")
 
 app.post('/api/users', (req, res) => {
     const body = req.body;
-
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({ msg : "All fields are required    "})
+    }
     users.push({ ...body, id: users.length + 1 });
 
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
         if (err) {
             return res.json({ status: "error" });
         }
-        return res.json({ status: "success", id: users.length });
+        return res.status(201).json({ status: "success", id: users.length });
     });
 });
 
