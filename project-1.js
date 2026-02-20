6399c3eba88b8d84062410cabaaddec4189ba98c
Rohittyagiii <tyagirohit698@gmail.com>
2026-02-20 22:35:02 +0530

    commit

--- project-1.js
+++ project-1.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const {connectMongoDb} = require('./connection')
+const userRouter = require('./routes/user')
+const {logReqRes} = require('./middleware')
 const app = express();
 
-// Connection
-mongoose.connect('mongodb://127.0.0.1:27017/project-1')
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log("Mongo Error", err));
+//connection 
+connectMongoDb('mongodb://127.0.0.1:27017/project-1')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(logReqRes('log.txt'))
 
-// =========================
-// HTML Route
-// =========================
-
-// Server
+//Routes
+app.use("./user",userRouter)
+ 
 app.listen(4000, () => {
     console.log('Server Started on Port 4000');
 });
--- routes/user.js
+++ routes/user.js
@@ -2,32 +2,27 @@ const express = require("express");
 
 const router = express.Router()
 
-router.get('/users', async (req, res) => {
-    const allDbUsers = await User.find({});
+// router.get('/users', async (req, res) => {
+//     const allDbUsers = await User.find({});
 
-    const html = `
-    <ul> 
-        ${allDbUsers.map(user => 
-            `<li>${user.firstName} - ${user.email}</li>`
-        ).join("")}
-    </ul>`;
+//     const html = `
+//     <ul> 
+//         ${allDbUsers.map(user => 
+//             `<li>${user.firstName} - ${user.email}</li>`
+//         ).join("")}
+//     </ul>`;
 
-    res.send(html);
-});
+//     res.send(html);
+// });
 
-
-// =========================
-// REST API
-// =========================
-
-// GET ALL USERS
-router.get('/api/users', async (req, res) => {
+ 
+router.get('/', async (req, res) => {
     const users = await User.find({});
     return res.json(users);
 });
 
 // GET, UPDATE, DELETE by ID
-router.route("/api/users/:id")
+router.route("/:id")
 
 // GET SINGLE USER
 router.get(async (req, res) => {
